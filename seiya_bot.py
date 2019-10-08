076665f7326ae7d23d597e68aec57c2e895fe729
mark104478 <49283068+mark104478@users.noreply.github.com>
2019-10-08 12:38:01 +0545

    Update seiya_bot.py

--- seiya_bot.py
+++ seiya_bot.py
@@ -6,6 +6,7 @@ from plugins import rand_joke, cryptoPrice, saucenao, translater, image, dice, y
 import os
 
 description = '''A simple discord bot using discord.py'''
+client = discord.Client()
 bot = commands.Bot(command_prefix='/', description=description)
 
 
@@ -31,7 +32,7 @@ async def youtube(*args : str):
 
 
 @bot.command(name='translate', description='Translates english to japanese or vice versa')
-async def translate(target : str, *args : str):
+async def translate(target : str, *args : str, ctx):
     message = ' '.join(args)
 
     if target == 'ja':
@@ -41,32 +42,32 @@ async def translate(target : str, *args : str):
         result = translater.getTranslation(target, message, config['DEFAULT']['GOOGLE_KEY'])
         embed = discord.Embed(title='English translation', description=result, color=0x7289da)
 
-    await bot.say(embed=embed)
+    await ctx.send(embed=embed)
 
 
 @bot.command(name='price', description='Retrieves the price of a cryptocurrency.')
-async def price(name : str ):
+async def price(name : str, ctx ):
     coin = cryptoPrice.getPrice(name,)
 
     if coin is None:
-        await bot.say('Unable to retrieve information ... ')
+        await ctx.send('Unable to retrieve information ... ')
         return
 
     message = '$ {}  |  ({}%) 24h'.format(coin['price_usd'], coin['percent_change_24h'])
     embed = discord.Embed(title=name, description=message, color=0x7289da)
 
-    await bot.say(embed=embed)
+    await ctx.send(embed=embed)
 
 @bot.command(name='joke', description='Returns a random joke to the user.')
-async def joke():
+async def joke(ctx):
     result = rand_joke.getJoke()
     embed = discord.Embed(title='Joke', description=result, color=0x7289da)
 
-    await bot.say(embed=embed)
+    await ctx.send(embed=embed)
 
 
 @bot.command(name='status', description='Returns the status of the bot.')
-async def status():
+async def status(ctx):
     day, hour, minutes, seconds = helper.getElapsedTime((time.time() - start))
     message = '{}d, {}h, {}m, {}s'.format(day, hour, minutes, seconds)
 
@@ -74,7 +75,7 @@ async def status():
     embed.add_field(name='Online', value=message)
     embed.add_field(name='Servers', value=str(len(bot.servers)))
 
-    await bot.say(embed=embed)
+    await ctx.send(embed=embed)
 
 
 bot.run(os.getenv('token'))
